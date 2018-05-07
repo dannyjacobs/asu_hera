c3538409805ae9fe0070c1826f885007a6642c36
Tyler Cox <tcox@enteprise.sese.asu.edu>
2018-05-07 13:04:41 -0700

    Update Image Generator file to run with multiple files

--- gen_image.py
+++ gen_image.py
@@ -32,13 +32,13 @@ def file_to_name(infile):
     #initialize variables for cleaning
     imgname=os.path.basename(infile)+ ".init.img"
     file_to_clean=os.path.basename(infile) + "split" + ".ms"
-    return (file_to_clean,imgname)
+    return (file_to_clean, imgname)
 
 def do_clean(file_to_clean, imgname):
     #cleaning the data
     clean(vis=file_to_clean, imagename=imgname, niter=500, weighting='briggs',robust=-0.5, imsize=[512,512], cell=['500arcsec'],mode='mfs',nterms=1,spw='0:150~900',mask="circle[[17h45m00.0s, -29d.00m.00.0s], 32000arcsec]")
 
-def do_band_pass(find_to_clean):
+def do_band_pass(file_to_clean):
     #apply bandpass on split data
     bc=calname(file_to_clean, "B")
     bandpass(vis=file_to_clean, spw="", minsnr=1, solnorm=F, bandtype="B", caltable=bc)
@@ -64,35 +64,93 @@ def file_3_names(file_2_clean):
 def clean_final(file_3_clean,imgnameFinal):
     clean(vis=file_3_clean, imagename=imgnameFinal, niter=5000, weighting='briggs', robust=-0.5, imsize=[512,512], cell=['250arcsec'],mode='mfs',nterms=1, spw='0:60~745', mask="circle[[17h45m00.0s, -29d00m00.0s], 32000arcsec]")
 
-if __name__ == "__main__":
-    infile = sys.argv[3]
-    print ('\n\nFlagging Data...\n\n')
+def make_img(infile):
+    img_dir = 'imgs'
+
+    print ('\nFlagging Data...\n')
     flag(infile)
-    print ('\n\nInitial Calibration...\n\n')
+
+    print ('\nInitial Calibration...\n')
     kc,gc = calinitial(infile)
     file_to_clean, imgname = file_to_name(infile)
-    print ('\n\nSplitting Columns...\n\n')
+
+    print ('\nSplitting Columns...\n')
     split_initial(infile)
-    print ('\n\nCleaning Data...\n\n')
+
+    print ('\nCleaning Data...\n')
     try:
         do_clean(file_to_clean, imgname)
     except:
         pass
-    print ('\n\nRunning Band Pass...\n\n')
+
+    print ('\nRunning Band Pass...\n')
     do_band_pass(file_to_clean)
-    print ('\n\nSplitting Columns...\n\n')
+
+    print ('\nSplitting Columns...\n')
     do_split(file_to_clean)
     file_2_clean,imgname2 = file_2_names(file_to_clean)
-    print ('\n\nCleaning Data...\n\n')
+
+    print ('\nCleaning Data...\n')
     try:
         do_clean(file_2_clean,imgname2)
     except:
         pass
-    print ('\n\nRunning Band Pass...\n\n')
+
+    print ('\nRunning Band Pass...\n')
     do_band_pass(file_2_clean)
     file_3_clean, imgnameFinal = file_3_names(file_2_clean)
-    print ('\n\nFinal Clean...\n\n')
+    imgnameFinal = os.path.join(img_dir,os.path.basename(imgnameFinal))
+
+    print ('\nFinal Clean...\n')
     try:
         clean_final(file_3_clean, imgnameFinal)
     except:
         pass
+
+def find_ms_files(path=None):
+    """
+
+    Finds all of the uvfits files in a given directory
+
+    Parameters
+    ----------
+    path : str
+        Folder path where the function looks for uvfits files.
+        Default is the current working directory.
+    polarization : str
+        Polarization of the uvfits files to search for.
+        Default is xx polarization
+
+    Returns
+    -------
+    array
+        An array of uvfits file names
+
+    """
+
+    if path is None:
+        path = os.getcwd()
+
+    folders = []
+
+    for folder in os.listdir(path):
+        if folder.endswith('.ms'):
+            folders.append(os.path.join(path,folder))
+
+    folders.sort()
+    return (folders)
+
+
+if __name__ == "__main__":
+    import time
+    folders = sys.argv[3:]
+    if folders is str:
+        make_img(folders)
+    else:
+        folders.sort()
+        print (folders[3:6])
+        for folder in folders[3:6]:
+            print (folder)
+            start = time.time()
+            make_img(folder)
+            print ("Run Time: " + str(time.time()-start) + " seconds")
