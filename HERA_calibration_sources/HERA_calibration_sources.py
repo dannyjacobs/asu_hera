c4424a03d7e79787920ebb8a858d1a19bcfe676a
Lily Whitler <whitler.lily@gmail.com>
2018-06-01 23:57:53 +0200

    Adding .gitignore file, implemeneting RA filtering for calibration sources

--- HERA_calibration_sources/HERA_calibration_sources.py
+++ HERA_calibration_sources/HERA_calibration_sources.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 import numpy as np
 import pandas as pd
+import re
 import matplotlib.pyplot as plt
 import healpy as hp
 from scipy.interpolate import interp1d
@@ -54,6 +55,8 @@ def beam_model(target_long=45, plot=False):
     power_interp            =   interp_beam(colat_interp)
 
     params, pcovs           =   curve_fit(gaussian_function, colat_interp, power_interp) # Fit a Gaussian to interpolated beam
+    def beam_model(x):
+        return np.exp(-0.5*((x - params[1])/params[2])**2)
 
     if plot:
         plt.close("all")
@@ -68,7 +71,7 @@ def beam_model(target_long=45, plot=False):
         plt.title("az = " + str(target_long))
         plt.tight_layout(); plt.show(block=False)
 
-    return params
+    return beam_model
 
 def read_data():
     """Read the data in the TGSS catalog FITS file.
@@ -87,11 +90,13 @@ def read_data():
 
     return data
 
-def filter_by_location(dec_range=7.):
+def filter_by_location(RA_range, dec_range=7.):
     """Filter the objects in the TGSS catalog by location.
 
     Parameters
     ----------
+    RA_range : tuple of strings
+        The range of right ascension to search, in the form (hh:mm:ss, hh:mm:ss), with the lower RA as the first element.
     dec_range : float, optional
         The range in which to search around the center of the HERA FoV, -30.7 degrees (default: 7.0).
 
@@ -102,15 +107,22 @@ def filter_by_location(dec_range=7.):
     """
 
     data                    =   read_data()
-    filtered_data           =   data[(data["dec"] >= (HERA_center - dec_range)) & (data["dec"] <= (HERA_center + dec_range))] # Filter by position
+    lo_RA                   =   [float(RA_range[0].split(":")[i]) for i in range(3)]
+    lo_RA_decimal           =   15. * (lo_RA[0] + lo_RA[1]/60. + lo_RA[2]/3600.)
+    hi_RA                   =   [float(RA_range[1].split(":")[i]) for i in range(3)]
+    hi_RA_decimal           =   15. * (hi_RA[0] + hi_RA[1]/60. + hi_RA[2]/3600.)
+    filtered_data           =   data[(data["RA"] >= lo_RA_decimal) & (data["RA"] <= hi_RA_decimal)] # Filter by RA
+    filtered_data           =   filtered_data[(filtered_data["dec"] >= (HERA_center - dec_range)) & (filtered_data["dec"] <= (HERA_center + dec_range))] # Filter by dec
 
     return filtered_data
 
-def calc_apparent_flux(dec_range=7.):
+def calc_apparent_flux(RA_range, dec_range=7.):
     """Calculate 'apparent' peak and total flux for each object in the HERA FoV.
 
     Parameters
     ----------
+    RA_range : tuple of strings
+        The range of right ascension to search, in the form (hh:mm:ss, hh:mm:ss), with the lower RA as the first element.
     dec_range : float, optional
         The range in which to search around the HERA FoV center of -30.7 degrees (default: 7.0).
 
@@ -120,20 +132,22 @@ def calc_apparent_flux(dec_range=7.):
         The names, right ascension, declination, peak aparent flux, and total apparent flux of the objects.
     """
 
-    data                    =   filter_by_location(dec_range=dec_range)
+    data                    =   filter_by_location(RA_range=RA_range, dec_range=dec_range)
     relative_loc            =   data["dec"] - HERA_center # Find location relative to center of HERA FoV
-    gauss_params            =   beam_model()
-    beam_correction         =   gaussian_function(relative_loc, gauss_params[0], gauss_params[1], gauss_params[2]) # Apply beam correction
+    beam_gauss              =   beam_model()
+    beam_correction         =   beam_gauss(relative_loc) # Apply beam correction
     data["Apparent peak flux"]  =   np.multiply(beam_correction, data["Peak flux"]) # Calculate apparent peak flux
     data["Apparent total flux"] =   np.multiply(beam_correction, data["Total flux"]) # Calculate apparent total flux
 
     return data
 
-def filter_by_flux(dec_range=7., min_flux=10.):
+def filter_by_flux(RA_range, dec_range=7., min_flux=10.):
     """Filter the objects in the TGSS catalog by flux.
 
     Parameters
     ----------
+    RA_range : tuple of strings
+        The range of right ascension to search, in the form (hh:mm:ss, hh:mm:ss), with the lower RA as the first element.
     min_flux : float, optional
         The minimum flux for objects to have (default: 10 Jy).
     dec_range : float, optional
@@ -147,19 +161,18 @@ def filter_by_flux(dec_range=7., min_flux=10.):
     """
 
     min_flux_mJy            =   min_flux * 1000. # Convert to mJy
-    data                    =   calc_apparent_flux(dec_range=dec_range) # Find the apparent flux
+    data                    =   calc_apparent_flux(RA_range=RA_range, dec_range=dec_range) # Find the apparent flux
     filtered_data           =   data[data["Apparent total flux"] >= min_flux_mJy] # Find objects with large flux
-
-    print("Number of objects found in a " + str(dec_range) + " degree band around the center of declination of the HERA FoV " + \
-            "with a minimum apparent total flux of " + str(min_flux) + " Jy: " + str(len(filtered_data))) # How many objects fit the criteria
 
     return filtered_data
 
-def add_fluxes(dec_range=7., min_flux=10.):
+def add_fluxes(RA_range, dec_range=7., min_flux=10.):
     """Add the fluxes of sources within circles centered around the bright sources.
 
     Parameters
     ----------
+    RA_range : tuple of strings
+        The range of right ascension to search, in the form (hh:mm:ss, hh:mm:ss), with the lower RA as the first element.
     min_flux : float, optional
         The minimum flux for individual objects to have (default: 10 Jy).
     dec_range : float, optional
@@ -172,7 +185,7 @@ def add_fluxes(dec_range=7., min_flux=10.):
     """
 
     data                        =   read_data() # The unfiltered data
-    filtered_data               =   filter_by_flux(dec_range=dec_range, min_flux=min_flux) # The filtered data (by location and flux)
+    filtered_data               =   filter_by_flux(RA_range=RA_range, dec_range=dec_range, min_flux=min_flux) # The filtered data (by location and flux)
     radius                      =   ang_res/2. # Radius of the circles enclosing each object
     lo_RA, hi_RA                =   filtered_data["RA"] - radius, filtered_data["RA"] + radius
     lo_dec, hi_dec              =   filtered_data["dec"] - radius, filtered_data["dec"] + radius
@@ -193,6 +206,14 @@ def add_fluxes(dec_range=7., min_flux=10.):
         flux.append(tot_flux)
 
     regions                     =   pd.DataFrame({"Name of Center": names, "RA": center_RA, "Dec": center_dec, "Total flux in region": flux}, columns=["Name of Center", "RA", "Dec", "Total flux in region"]) # Create a dataframe
+
+    # Print information about chosen parameters
+    print("\nRight ascension:\n\tLower: " + lo_RA_disp + " \n\tUpper: " + hi_RA_disp)
+    print("Declination:\n\tLower: " + str(HERA_center - dec_range) + " degrees\n\tUpper: " + str(HERA_center + dec_range) + " degrees")
+    print("Minimum flux: " + str(min_flux) + " Jy")
+    print("Number of regions found: " + str(len(regions)) + "\n")
+    print(regions)
+
     return regions
 
 def find_nearest(array, value):
@@ -218,29 +239,49 @@ def gaussian_function(x, amp, mu, sigma):
     return amp*np.exp(-0.5*((x - mu)/sigma)**2)
 
 if __name__ == "__main__":
-    # Limits on location and minimum flux
+    # Limit on RA
+    print("What range of right ascension would you like to search in?")
+    flag_lo                     =   False
+    rexp                        =   re.compile("^[0-9][0-9]:[0-9][0-9]:[0-9][0-9]$") # To check format of string
+    while not flag_lo:
+        lo_RA                           =   input("\tLower [hh:mm:ss]: ")
+        # Check if format of lo_RA string is correct
+        if rexp.match(lo_RA):
+            flag_lo                             =   True
+            lo_RA_disp                          =   lo_RA[:2] + "h" + lo_RA[3:5] + "m" + lo_RA[6:] + "s"
+        else: print("Please input a right ascension in the form hh:mm:ss.")
+
+    flag_hi                     =   False
+    while not flag_hi:
+        hi_RA                           =   input("\tUpper [hh:mm:ss]: ")
+        # Check if format of hi_RA string is correct
+        if rexp.match(hi_RA):
+            flag_hi                             =   True
+            hi_RA_disp                          =   hi_RA[:2] + "h" + hi_RA[3:5] + "m" + hi_RA[6:] + "s"
+        else: print("Please input a right ascension in the form hh:mm:ss.")
+
+    # Limit on declination
     dec_range                   =   input("What declination range around the center of the HERA FoV " + \
                                                 "would you like to search in? [default: 7 degrees] ")
     if dec_range == "":
         dec_range                       =   "7." # Default declination range
-
-    # Try to convert string to number
+    # Try to convert to number
     try:
         dec_range                       =   float(dec_range)
     except ValueError:
         print("That is not a valid number. Defaulting to range of 7 degrees.")
         dec_range                       =   7.
 
-    min_flux                    =   input("What minimum flux should the objects have? [default: 10 Jy] ")
+    # Limit on minimum flux
+    min_flux                    =   input("What minimum flux should the objects have (Jy)? [default: 10 Jy] ")
     if min_flux == "":
         min_flux                        =   "10." # Default minimum flux
-
-    # Try to convert string to number
+    # Try to convert to number
     try:
         min_flux                        =   float(min_flux)
     except ValueError:
         print("That is not a valid number. Defaulting to minimum flux of 10 Jy.")
         min_flux                        =   10.
 
-    regions                     =   add_fluxes(dec_range=dec_range, min_flux=min_flux)
-    print(regions)
+    # Find regions
+    regions                     =   add_fluxes(RA_range=(lo_RA, hi_RA), dec_range=dec_range, min_flux=min_flux)
